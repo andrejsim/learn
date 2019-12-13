964279563a681273031c58ce45a67e6d865f10b5
Andrej <Sim>
2019-12-13 16:55:27 +0100

    weekend.

--- parquet/load_print.py
+++ parquet/load_print.py
@@ -6,21 +6,24 @@ import pyarrow.parquet as pq
 
 def size_mb(size):
     return size / (1024.0*1024.0)
-gi
+
 
 def main():
     f1 = '../data/e024b429-3fb1-4a6d-b4e6-23fe5eaadfc5'
     f2 = '../data/468cd686-0b96-4296-92ff-45f46c73b90e'
-    f3 = 'dataset.parquet'
+
+    fp1 = 'dataset1.parquet'
+    fp2 = 'dataset2.parquet'
+    fp3 = 'dataset3.parquet'
 
     ds1 = xr.open_dataset(f1)
-    print(ds1.keys())
+    pq.write_table(pa.Table.from_pandas(ds1.to_dataframe()), fp1)
 
     ds2 = xr.open_dataset(f2)
-    print(ds2.keys())
+    pq.write_table(pa.Table.from_pandas(ds2.to_dataframe()), fp2)
 
     ds = ds1.merge(ds2)
-    print(ds.info())
+    ds.to_netcdf("dataset3.nc")
 
     # dask required TODO...
     # with xr.open_mfdataset('../data/*') as ds:
@@ -30,8 +33,9 @@ def main():
     table = pa.Table.from_pandas(df)
     print(table.to_pandas())
 
-    pq.write_table(table, f3)
-    for f in [f1, f2, f3]:
+    pq.write_table(table, fp3)
+
+    for f in [f1, fp1, f2, fp2,  fp3]:
         print("{}           {} MB".format(f, size_mb(os.path.getsize(f))))
 
 
